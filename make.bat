4915f22a4423e3c22e1b6ab813065e0952e01a17
Persune <gumball2415@gmail.com>
2023-06-16 12:47:36 +0800

    Fix for Github Actions builds

--- make.bat
+++ make.bat
@@ -1,4 +1,3 @@
 mkdir output
 ca65 -g -o full_palette_persune.o full_palette.s -l output/list.txt
 ld65 -v -C nrom256.cfg --dbgfile output/full_palette_persune.dbg full_palette_persune.o -o output/full_palette_persune.nes
-pause
